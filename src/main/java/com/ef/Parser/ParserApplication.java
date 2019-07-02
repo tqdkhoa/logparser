0f730e20e942ba65d885068f6c23f0567f95dc59
tqdkhoa <tqdkhoa@tma.com.vn>
2019-07-02 18:09:38 +0700

    Remove Unused Import

--- src/main/java/com/ef/Parser/ParserApplication.java
+++ src/main/java/com/ef/Parser/ParserApplication.java
@@ -1,11 +1,7 @@
 package com.ef.Parser;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
